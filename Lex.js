1b4750f7823c90d2f0d56a30aa6c6d52f5bffcce
Blackwerecat <31685571+Blackwerecat@users.noreply.github.com>
2017-12-04 13:14:06 -0800

    Update Lex.js

--- Lex.js
+++ Lex.js
@@ -1,1 +1,19 @@
+function type_of(char) {
+	if (Number(char))
+		return 'int';
+	else if ('()'.includes(char))
+		return 'par';
+	else if ('.'.inclues(char))
+		return 'dot';
+	else if ('+-*/'.includes(char))
+		return 'opr';
+	else
+		return 'char';
+}
 
+
+function Lex(string) {
+	var prev = '';
+	for (char in string.split('')) {
+		
+}
